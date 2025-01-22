801817ed072ce12fb24678f864291d95d0c9857b
Teigland <samuel.teigland@nm.com>
2025-01-21 21:15:42 -0600

    Forgot to capitalize a word

--- src/components/global/navbar/Navbar.jsx
+++ src/components/global/navbar/Navbar.jsx
@@ -47,7 +47,7 @@ export default function Navbar() {
                     Contact Us
                 </a>
                 <a href='#'>
-                    blog
+                    Blog
                 </a>
             </div>
         </div>
