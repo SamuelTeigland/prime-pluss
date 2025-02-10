b6c82da708b64f88c7d7b2bbd61aa775c60af983
Teigland <samuel.teigland@nm.com>
2025-02-10 08:33:16 -0600

    added stuff to header

--- src/components/home/call/Call.jsx
+++ src/components/home/call/Call.jsx
@@ -3,8 +3,16 @@ import './call.css'
 export default function Call() {
     return (
         <div className='call__container'>
-            <h2 className='call__header'>Call now!</h2>
-            <p className='call__number'>(302) 275-4692</p>
+            <div className='call__subcontainer'>
+                <div>
+                    <h2 className='call__header'>Call now!</h2>
+                    <p className='call__number'>(302) 275-4692</p>
+                </div>
+                <div>
+                    <h2 className='call__servicing--header'>Servicing:</h2>
+                    <p className=' call__servicing--number'>All of Delaware, Eastern Maryland, Southeastern Pennsylvania.</p>
+                </div>
+            </div>
         </div>
     )
 }
