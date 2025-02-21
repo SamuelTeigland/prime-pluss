d16067d23e752b55c65704a2c9f6e0f1b6ee7c9a
Teigland <samuel.teigland@nm.com>
2025-02-20 21:46:25 -0600

    added more to navbar

--- src/components/global/navbar/Navbar.jsx
+++ src/components/global/navbar/Navbar.jsx
@@ -14,16 +14,27 @@ export default function Navbar() {
                 </a>
                 <div className='navbar__link--container'>
                     <a href='#'>
-                        Services
+                        Attic
+                    </a>
+                    <a href='#'>
+                        Crawl Space
+                    </a>
+                    <a href='#'>
+                        Other Services
                     </a>
                     <Link to="/about-us">
-                        Why Prime Pluss?
+                        Our Work
                     </Link>
+                    <a href='#'>
+                        Financing
+                    </a>
                     <Link to='/contact-us'>
-                        Contact Us
+                        Contact
                     </Link>
-                    <a href="#">
-                        Blog
+                    <a href='#'>
+                        <button className='button__secondary navbar__button'>
+                            Get a Quote
+                        </button>
                     </a>
                     <div className='hamburger__container' onClick={
                         () => {
@@ -36,16 +47,25 @@ export default function Navbar() {
             </div>
             <div className={ isVisible === true ? 'hamburger__link--container shown__menu' : 'hamburger__link--container hidden__menu' }>
                 <a href='#' onClick={ () => { setVisible(!isVisible); } }>
-                    Services
+                    Attic
+                </a>
+                <a href='#' onClick={ () => { setVisible(!isVisible); } }>
+                    Crawl Space
+                </a>
+                <a href='#' onClick={ () => { setVisible(!isVisible); } }>
+                    Other Services
                 </a>
                 <Link to="/about-us" onClick={ () => { setVisible(!isVisible); } }>
-                    Why Prime Pluss?
-                </Link>
-                <Link to='/contact-us' onClick={ () => { setVisible(!isVisible); } }>
-                    Contact Us
+                    Our Work
                 </Link>
                 <a href='#' onClick={ () => { setVisible(!isVisible); } }>
-                    Blog
+                    Financing
+                </a>
+                <Link to='/contact-us' onClick={ () => { setVisible(!isVisible); } }>
+                    Contact
+                </Link>
+                <a id='navbar__special--link' href='#' onClick={ () => { setVisible(!isVisible); } }>
+                    Get a Quote
                 </a>
             </div>
         </div>
